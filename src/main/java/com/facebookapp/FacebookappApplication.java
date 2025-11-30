02d1af2b6520d71f760e2895bb0b88d2264d421c
Saranya <saranyabs1804@gmail.com>
2025-11-30 23:38:44 +0530

    Initial commit

---
+++ src/main/java/com/facebookapp/FacebookappApplication.java
@@ -0,0 +1,14 @@
+package com.facebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookappApplication.class, args);
+	}
+
+}
+	
